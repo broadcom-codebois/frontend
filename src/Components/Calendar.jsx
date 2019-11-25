8de9294d1f6d4130a73fefda1118186850b736ba
Matyáš Boháček <matyas.bohacek@matsworld.io>
2019-11-25 14:36:39 +0100

    Repairs the css link

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
-import './index.css'
+import '../index.css'
 
 export default class Calendar extends React.Component {
 
