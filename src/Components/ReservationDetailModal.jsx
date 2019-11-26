98223043f6fe763cd3d148ff8a5d7ef6a365b5d9
Jan Kaifer <kaifer741@gmail.com>
2019-11-26 11:49:26 +0100

    fixed dependencies

--- src/Components/ReservationDetailModal.jsx
+++ src/Components/ReservationDetailModal.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -47,20 +46,7 @@ export default function ReservationDetailModal() {
       <button type="button" onClick={handleOpen}>
         Open Modal
       </button>
-      <Modal
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        open={open}
-        onClose={handleClose}
-      >
-        <div style={modalStyle} className={classes.paper}>
-          <h2 id="simple-modal-title">Text in a modal</h2>
-          <p id="simple-modal-description">
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </p>
-          <ReservationDetailModal />
-        </div>
-      </Modal>
+
     </div>
   );
 }
