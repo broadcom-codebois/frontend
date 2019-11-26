8538b7e1aa19a30d62b666001bd83ec61ee222cb
Matyáš Boháček <matyas.bohacek@matsworld.io>
2019-11-26 15:33:54 +0100

    Adds table layouts page

---
+++ src/Pages/TableLayouts.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Box, Container, makeStyles } from '@material-ui/core'
+
+import Timeline from 'Components/Timeline'
+
+const useStyle = makeStyles({
+  container: {
+    backgroundColor: '#f7ffff',
+  },
+})
+
+const TableLayouts = () => {
+  const c = useStyle()
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default TableLayouts
