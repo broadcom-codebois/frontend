4ffb42b99f2215a547a620ed86c6d9cfcc9c7888
Jan Kaifer <kaifer741@gmail.com>
2019-11-27 13:07:06 +0100

    commit

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -24,7 +24,21 @@ import { useEvents, useCreateEvent } from 'Hooks'
 
 import './calendar-style.scss'
 
-const initialEventState = {}
+const initialEventState = {
+  name: '',
+  description: '',
+  author: '',
+  north: true,
+  south: true,
+  begin_time: dayjs()
+    .add(1, 'day')
+    .valueOf(),
+  end_time: dayjs()
+    .add(2, 'day')
+    .valueOf(),
+  layout: 0,
+  approved: false,
+}
 
 const useStyle = makeStyles({
   roomPicker: {
@@ -173,7 +187,7 @@ const Calendar = () => {
                   )
                   .join(', ')}
               </Typography>
-              <Typography>Owner: {visibleInfoDialog.owner}</Typography>
+              <Typography>Owner: {visibleInfoDialog.author}</Typography>
             </DialogContent>
           </>
         )}
@@ -202,13 +216,30 @@ const Calendar = () => {
               </Grid>
               <Grid className={c.dateRangePicker}>
                 <InputLabel htmlFor="datePicker">Time range</InputLabel>
-                <DatePicker />
+                <DatePicker
+                  value={{
+                    start: dayjs(newEventData.begin_time).format(
+                      'YYYY-M-D HH:mm'
+                    ),
+                    end: dayjs(newEventData.end_time).format('YYYY-M-D HH:mm'),
+                  }}
+                  onChange={value =>
+                    setNewEventData(d => ({
+                      ...d,
+                      begin_time: dayjs(value.start).valueOf(),
+                      end_time: dayjs(value.end).valueOf(),
+                    }))
+                  }
+                />
               </Grid>
               <Grid item>
-                <TextField label="Organiser name" />
-              </Grid>
-              <Grid item>
-                <TextField type="email" label="Organiser email" />
+                <TextField
+                  label="Organiser name"
+                  value={newEventData.author}
+                  onChange={(e, val) =>
+                    setNewEventData(d => ({ ...d, name: val }))
+                  }
+                />
               </Grid>
               <Grid item>
                 <TextField label="Note" multiline />
