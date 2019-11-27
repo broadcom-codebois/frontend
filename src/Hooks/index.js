e6e16ba67568c5dae22208189bfe344b4e3dbe14
Jan Kaifer <kaifer741@gmail.com>
2019-11-27 11:30:53 +0100

    adde use events

---
+++ src/Hooks/index.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import dayjs from 'dayjs'
+
+const api = axios.create({
+  baseURL: 'https://codeweek2019.kaifer.cz/api/',
+  timeout: 2000,
+})
+
+export const useEvents = () => {
+  const [apiState, setApiState] = useState({
+    events: undefined,
+    fetching: false,
+    error: undefined,
+    lastRequest: dayjs().valueOf(),
+  })
+
+  if (
+    !apiState.fetching &&
+    (apiState.events === undefined ||
+      apiState.lastRequest < dayjs().valueOf() - 1000)
+  ) {
+    setApiState(s => ({ ...s, fetching: true }))
+    api
+      .get('events')
+      .then(response => {
+        setApiState(s => ({
+          ...s,
+          fetching: false,
+          events: response.data,
+          error: undefined,
+          lastRequest: dayjs().valueOf(),
+        }))
+      })
+      .catch(error => {
+        setApiState(s => ({
+          ...s,
+          fetching: false,
+          error: error,
+          lastRequest: dayjs().valueOf(),
+        }))
+      })
+  }
+
+  const forceRefresh = () => setApiState(s => ({ ...s, lastRequest: 0 }))
+  const events = apiState.events === undefined ? [] : apiState.events
+
+  return [events, forceRefresh]
+}
